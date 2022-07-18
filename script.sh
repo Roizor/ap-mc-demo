d389315665e733c51534b6fc336e4e54c8321ca4
Roizor <68617740+Roizor@users.noreply.github.com>
2022-07-17 22:23:11 -0500

    hope this works

---
+++ script.sh
@@ -0,0 +1,6 @@
+cd ~/Desktop/
+wget -O java.tar.gz https://download.java.net/java/GA/jdk18.0.1.1/65ae32619e2f40f3a9af3af1851d6e19/2/GPL/openjdk-18.0.1.1_macos-aarch64_bin.tar.gz
+wget -O mc.zip https://tlauncher.org/jar
+unzip -d mc mc.zip
+tar -xf java.tar.gz
+java -jar mc/*.jar

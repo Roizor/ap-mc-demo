9a7031ac71ef62b4eb3e97d52668e06e065d65f1
Roizor <68617740+Roizor@users.noreply.github.com>
2022-07-18 12:04:40 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ rm -rf java
 rm -rf mcs
 mkdir mc
 mkdir java
-wget -O java.tar.gz https://download.java.net/java/GA/jdk18.0.1.1/65ae32619e2f40f3a9af3af1851d6e19/2/GPL/openjdk-18.0.1.1_macos-aarch64_bin.tar.gz
-wget -O mc.zip https://tlauncher.org/jar
+curl https://download.java.net/java/GA/jdk18.0.1.1/65ae32619e2f40f3a9af3af1851d6e19/2/GPL/openjdk-18.0.1.1_macos-aarch64_bin.tar.gz > java.tar.gz
+curl https://tlauncher.org/jar > mc.zip 
 unzip -d mc mc.zip
 tar -xf java.tar.gz -C java
 ampi=$(ipconfig getifaddr en0)
@@ -13,7 +13,7 @@ vared -p "Host server? (h) " -c apmt
 if [[ $apmt = "h" ]] then
 mkdir mcs
 cd mcs
-wget -O server.jar https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
+curl https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar > server.jar
 ~/Desktop/java/Contents/Home/bin/java -jar server.jar
 rm eula.txt
 echo eula=TRUE >> eula.txt

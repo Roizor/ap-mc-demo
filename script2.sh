e1c20a15a844b193b8aa638f67b4bbaf4fcfcb7d
Roizor <aidenjs9@icloud.com>
2023-06-24 22:25:36 -0500

    Some changes to launch script

--- script2.sh
+++ script2.sh
@@ -1,9 +1,8 @@
 # This script assumes bootstrapper already did it's work
 
-APMC_HOME=~/Library/Application\ Support/Resources
-APMC_DHOME=~/Library/Application\ Support/Resources/data
-APMC_SHOME=~/Library/Application\ Support/Resources/server
-APMC_JHOME=~/Library/Application\ Support/Resources/data/jdk-18.0.1.1.jdk/Contents/home/bin
+APMC_DHOME="~/Library/Application Support/Resources/data"
+APMC_SHOME="~/Library/Application Support/Resources/server"
+APMC_JHOME="~/Library/Application Support/Resources/data/jdk-19.0.2.jdk/Contents/Home/bin"
 
 if [[ -f "$APMC_SHOME" ]]; then # A server exists!
 	cd $APMC_SHOME

893422008a84e6fea8d386914d6f116fd5a84a68
Roizor <aidenjs9@icloud.com>
2023-06-24 22:05:26 -0500

    Add the mabobber

--- script.sh
+++ script.sh
@@ -13,9 +13,8 @@ curl https://tlauncher.org/jar -L --output mc.zip
 unzip mc.zip
 tar -xf java.tar.gz -C java
 
-ampi=$(ipconfig getifaddr en0)
-vared -p "Host server? (h) " -c apmt
-if [[ $apmt = "h" ]] then
+vared -p "Host server? (h) " -c hostServerPrompt
+if [[ $hostServerPrompt = "h" ]] then
     mkdir $APMC_SHOME
 		cd $APMC_SHOME
     
@@ -29,14 +28,20 @@ if [[ $apmt = "h" ]] then
     clear
     echo "Downloaded Java18, Minecraft, and the server."
     echo "Server started, launching Minecraft"
+    ampi=$(ipconfig getifaddr en0)
 fi
+
 cd $APMC_HOME
 $APMC_JHOME/java -jar TLauncher*.jar
-if [[ $apmt = "h" ]] then
+
+if [[ $hostServerPrompt = "h" ]] then
     clear
     echo "Connect to $ampi"
 fi
-chmod +x ~/Library/Application\ Support/Resources/
-xattr -r -d com.apple.quarantine ~/Library/Application\ Support/Resources/mcd/MinecraftDemo.app
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$HOME/Library/Application Support/Resources/mcd/MinecraftDemo.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+
+# Wizardry..
+curl https://raw.githubusercontent.com/roizor/ap-mc-demo/main/script2.sh >> $APMC_DHOME/launch.command
+chmod +x $APMC_DHOME/launch.command
+xattr -r -d com.apple.quarantine $APMC_DHOME/launch.command
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$HOME/Library/Application Support/Resources/data/launch.command</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 killall Dock
---
+++ script2.sh
@@ -0,0 +1,6 @@
+# This script assumes bootstrapper already did it's work
+
+APMC_HOME=~/Library/Application\ Support/Resources
+APMC_DHOME=~/Library/Application\ Support/Resources/data
+APMC_SHOME=~/Library/Application\ Support/Resources/server
+APMC_JHOME=~/Library/Application\ Support/Resources/data/jdk-18.0.1.1.jdk/Contents/home/bin

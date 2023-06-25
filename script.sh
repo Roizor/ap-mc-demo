549bd3f49099f69d2fe3b8b4229f0298a8e897d5
Roizor <aidenjs9@icloud.com>
2023-06-24 22:21:48 -0500

    oops

--- script.sh
+++ script.sh
@@ -39,7 +39,7 @@ xattr -r -d com.apple.quarantine $APMC_DHOME/launch.command
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$HOME/Library/Application Support/Resources/data/launch.command</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
 killall Dock
 
-cd $APMC_HOME
+cd $APMC_DHOME
 $APMC_JHOME/java -jar TLauncher*.jar
 
 if [[ $hostServerPrompt = "h" ]] then

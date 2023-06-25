1e08ad3c160e3c8614074af13ec091d1a3853b7d
Roizor <aidenjs9@icloud.com>
2023-06-24 22:44:21 -0500

    Do formatting on scripts

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ APMC_SHOME=~/Library/Application\ Support/Resources/server
 APMC_JHOME=~/Library/Application\ Support/Resources/data/jdk-19.0.2.jdk/Contents/Home/bin
 
 if [[ $(uname -p) == 'arm' ]]; then
-  JDK_LINK="https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_macos-aarch64_bin.tar.gz"
+    JDK_LINK="https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_macos-aarch64_bin.tar.gz"
 else
-  JDK_LINK="https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_macos-x64_bin.tar.gz"
+    JDK_LINK="https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_macos-x64_bin.tar.gz"
 fi
 
 rm -rf $APMC_DHOME $APMC_SHOME $APMC_HOME
@@ -22,7 +22,7 @@ tar -xf java.tar.gz
 vared -p "Host server? (h) " -c hostServerPrompt
 if [[ $hostServerPrompt = "h" ]] then
     mkdir $APMC_SHOME
-		cd $APMC_SHOME
+    cd $APMC_SHOME
     
     curl https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar --output server.jar
     $APMC_JHOME/java -jar server.jar # start server once to make eula
--- script2.sh
+++ script2.sh
@@ -5,8 +5,8 @@ APMC_SHOME="$HOME/Library/Application Support/Resources/server"
 APMC_JHOME="$HOME/Library/Application Support/Resources/data/jdk-19.0.2.jdk/Contents/Home/bin"
 
 if [[ -f "$APMC_SHOME" ]]; then # A server exists!
-	cd $APMC_SHOME
-	screen -dmS mcs $APMC_JHOME/java -jar server.jar
+    cd $APMC_SHOME
+    screen -dmS mcs $APMC_JHOME/java -jar server.jar
 fi
 
 cd $APMC_DHOME
